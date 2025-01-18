835526e90bd80d793e123f47ea316678d83586f1
bantoinese83 <b.antoine.se@gmail.com>
2025-01-18 14:31:29 -0500

    implemented middlewares.py

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,6 @@
 from sqlmodel import SQLModel, create_engine, Session
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./backend/test.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
